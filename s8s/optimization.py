6cbe3c0f0ba7a688f8057ece3f4a6fb78fa1d7b2
PngWnA <gnu@korea.ac.kr>
2022-07-01 16:43:02 +0900

    [Test] Added test case (tar-1.0)

--- s8s/optimization.py
+++ s8s/optimization.py
@@ -1,30 +1,49 @@
 import os
 
+from tomlkit import key
+
+# equivalent to grep
+def ogrep(data, keyword):
+    for line in data:
+        if line.startswith(keyword):
+            print(line)
+    return
+
+
+# Simple heuristic to guess the opt level
+def guess_opt_level(path):
+    prefix = path[0]
+    res = {}
+
+
+    if "Makefile" in path[2]:
+        print("[+] Makefile in root directory")
+        raw = open(prefix+"/Makefile", "r").readlines()
+        if ogrep(raw, "CFLAGS"):
+            print("wow")
+    if "config.status" in path[2]:
+        print("[+] config.status in root directory")
+    if "configure" in path[2]:
+        print("[+] configure in root directory")
+    if "Makefile.am" in path[2]:
+        print("[+] Makefile.am in root directory")
+    if "configure.ac" in path[2]:
+        print("[+] configure.ac in root directory")
+    return
+
+
+
 # Return optimization level of main target package
 def check_opt_level():
     lst = list(os.walk("./"))
     
     root = lst[1]
-    prefix = root[0]
 
     ''' 
     Check from
     * Makefile -> config.status -> Configure -> Makefile.am -> Configure.ac
     '''
     
-    res = {}
-    
-    if "Makefile" in root[2]:
-        print("[+] Makefile in root directory")
-        raw = open(prefix+"/Makefile", "r").read()
-        print(raw)
-    if "config.status" in root[2]:
-        print("[+] config.status in root directory")
-    if "configure" in root[2]:
-        print("[+] configure in root directory")
-    if "Makefile.am" in root[2]:
-        print("[+] Makefile.am in root directory")
-    if "configure.ac" in root[2]:
-        print("[+] configure.ac in root directory")
-
+    res = guess_opt_level(root)
     return
+
